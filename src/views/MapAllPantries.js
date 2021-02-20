7e3b2d345c62fb7fff5f85917cc7e0dc529856f8
Scarcrux <reg@scarcrux.com>
2021-02-20 15:51:09 -0500

    Add Single Pantry Map View - #55

--- src/views/MapAllPantries.js
+++ src/views/MapAllPantries.js
@@ -12,33 +12,43 @@ import {
   Row,
   Col,
 } from "reactstrap";
+import { pantryListReducer } from "reducers/pantryReducers";
+import { listPantries } from "actions/pantryActions";
+import { useSelector, useDispatch } from 'react-redux';
 
 // core components
 import HeaderIndex from "../components/HeaderIndex.js";
 import MyMapComponent from "../components/MyMapComponent";
 //import DefaultFooter from "components/Footers/DefaultFooter.js";
 
-function MapAllPantries() {
+function MapAllPantries(props) {
+  const dispatch = useDispatch();
+  const lists = useSelector(state => state.pantryList);
+  const { loading, error, pantries } = lists;
+
   React.useEffect(() => {
+    dispatch(listPantries());
     return function cleanup() {
     };
   }, []);
   return (
-    <>
+    <>{ loading ? <div>Loading...</div> :
+       error ? <div>{error}</div> :
       <div className="wrapper">
+
         <div className="section section-heat-map">
           <Container>
             <Row>
               <Col style={{color:"black"}}className="text-center">
                 A Map to visualize the pantry locations on the Google Map
-              <MyMapComponent></MyMapComponent>
+              <MyMapComponent pantries={pantries} />
               </Col>
             </Row>
           </Container>
         </div>
 
         {/*<DefaultFooter />*/}
-      </div>
+      </div>}
     </>
   );
 }
