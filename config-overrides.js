b7dd4eecbb3b6aca49a9683e79c2953a2c26612d
Scarcrux <reg@scarcrux.com>
2021-03-01 02:42:53 -0500

    Add Netlify Config File - #66

--- config-overrides.js
+++ config-overrides.js
@@ -1,27 +1,3 @@
-const {override} = require('customize-cra');
-const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
-
-const cspConfigPolicy = {
-    'default-src': "'*'",
-    'base-uri': "'*'",
-    'object-src': "'*'",
-    'script-src': ["'*'"],
-    'style-src': "'*'",
-    'worker-src': "'*'"
-};
-
-function addCspHtmlWebpackPlugin(config) {
-    if(process.env.NODE_ENV === 'production') {
-        config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
-    }
-
-    return config;
-}
-
-module.exports = {
-    webpack: override(addCspHtmlWebpackPlugin),
-};
-
 module.exports = function override(webpackConfig) {
   webpackConfig.module.rules.push({
     test: /\.mjs$/,
