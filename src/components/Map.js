71f28cf9ff5890c6f5e39cd5b87348fb114610f6
Scarcrux <reg@scarcrux.com>
2021-03-02 21:24:40 -0500

    Update Mapbox Map - #66

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,36 +1,28 @@
 import React, { useEffect, useRef, useState } from "react";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-
-const styles = {
-  width: "80vw",
-  height: "80vh"
-};
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import { render } from "@testing-library/react";
 
 const Map = () => {
-  const [map, setMap] = useState(null);
-  const mapContainer = useRef(null);
-
-  useEffect(() => {
-    mapboxgl.accessToken = "pk.eyJ1Ijoic2NhcmNydXgiLCJhIjoiY2pyOW5jeHRqMGpsNzQ0cGQzNXdwbmI2ZiJ9.ig8MDQnhZtsaykzp9el9iA";
-    const initializeMap = ({ setMap, mapContainer }) => {
-      const map = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: "mapbox://styles/scarcrux/ckkt289a41qyj17pkvjxc9eck", // stylesheet location
-        center: [-73.972951, 40.653124],
-        zoom: 11.44
+       const Map = ReactMapboxGl({
+        accessToken:
+          'pk.eyJ1Ijoic2NhcmNydXgiLCJhIjoiY2pyOW5jeHRqMGpsNzQ0cGQzNXdwbmI2ZiJ9.ig8MDQnhZtsaykzp9el9iA'
       });
 
-      map.on("load", () => {
-        setMap(map);
-        map.resize();
-      });
-    };
+      return (
+      <Map
+        style="mapbox://styles/scarcrux/ckkt289a41qyj17pkvjxc9eck"
+        center={[-73.972951, 40.653124]}
+        zoom={[11.44]}
+        containerStyle={{
+          height: '80vh',
+          width: '80vw'
+        }}
+      >
 
-    if (!map) initializeMap({ setMap, mapContainer });
-  }, [map]);
 
-  return <div id="container-map" ref={el => (mapContainer.current = el)} style={styles}/>;
+      </Map>
+      );
 };
 
 export default Map;
