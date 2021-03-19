b8c567824aa83020656f676e1b5df7fcc10aef02
Scarcrux <reg@scarcrux.com>
2021-03-19 11:17:10 -0400

    Add Switch to Routes - #66

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import NavbarIndex from './components/NavbarIndex';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import {
   AddOrganization,
   AddPantry,
@@ -32,30 +32,30 @@ function App() {
       <div className="App">
         <header className="App-header">
           <div className="content-wrap">
-          <Route exact path="/" component={LandingPage} />
-          <Route exact path="/addorg" component={AddOrganization} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/donate" component={Donate} />
-
-          <Route exact path="/info" component={MapHouseholdIncome} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path='/myorgs' component={MyOrgs} />
-
-          <Route path='/org/:id' exact render={({match}) =>  <Organization id={match.params.id} /> } />         <Route path='/org/:id/addpantry' exact render={({match}) =>  <AddPantry id={match.params.id} /> } />
-          <Route path='/org/edit/:id' exact render={({match}) =>  <EditOrganization id={match.params.id} /> } />
-          <Route exact path="/orgs" component={Organizations} />
-          <Route path='/pantry/:id' exact render={({match}) =>  <Pantry id={match.params.id} /> } />
-          <Route path='/pantry/edit/:id' exact render={({match}) =>  <EditPantry id={match.params.id} /> } />
-          <Route exact path="/pantries" component={Pantries} />
-          <Route exact path="/pantrymap" component={MapPantries} />
-          <Route exact path="/privacy" component={Privacy} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/terms" component={Terms} />
-          <Route path='/vieworg/:id' exact render={({match}) =>  <OrganizationPublic id={match.params.id} /> } />
-          <Route component={NotFound} />
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/addorg" component={AddOrganization} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/donate" component={Donate} />
+              <Route exact path="/info" component={MapHouseholdIncome} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path='/myorgs' component={MyOrgs} />
+              <Route path='/org/:id' exact render={({match}) =>  <Organization id={match.params.id} /> } />         <Route path='/org/:id/addpantry' exact render={({match}) =>  <AddPantry id={match.params.id} /> } />
+              <Route path='/org/edit/:id' exact render={({match}) =>  <EditOrganization id={match.params.id} /> } />
+              <Route exact path="/orgs" component={Organizations} />
+              <Route path='/pantry/:id' exact render={({match}) =>  <Pantry id={match.params.id} /> } />
+              <Route path='/pantry/edit/:id' exact render={({match}) =>  <EditPantry id={match.params.id} /> } />
+              <Route exact path="/pantries" component={Pantries} />
+              <Route exact path="/pantrymap" component={MapPantries} />
+              <Route exact path="/privacy" component={Privacy} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/terms" component={Terms} />
+              <Route path='/vieworg/:id' exact render={({match}) =>  <OrganizationPublic id={match.params.id} /> } />
+              <Route component={NotFound} />
+            </Switch>
           </div>
-             <Footer />
-          </header>
+          <Footer />
+        </header>
       </div>
     </BrowserRouter>
   );
